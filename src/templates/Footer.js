5d30ca363ed70d0168fa9beb4b674cee88fac82d
fenil nakrani <fenilnakrani64@gmail.com>
2024-04-17 11:35:35 -0400

    updates

--- src/templates/Footer.js
+++ src/templates/Footer.js
@@ -30,20 +30,6 @@ const Footer = () => {
                 </div>
                 <div className="text-white mb-3 mb-md-0">
                     Server Version: {serverVersion}
-                {/* </div>
-                <div> */}
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-google"></i>
-                    </a>
-                    <a href="#!" className="text-white">
-                        <i className="fab fa-linkedin-in"></i>
-                    </a>
                 </div>
             </footer>
         </>
